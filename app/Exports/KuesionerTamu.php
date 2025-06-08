535e03704cc393ee89e91c1c300a954865447642
adeleeeeyyyy <mitsuhaadly@gmail.com>
2025-06-08 23:39:02 +0700

    nmbahin export tamu

---
+++ app/Exports/KuesionerTamu.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Exports;
+
+use App\Models\KuesionerTamu;
+use Maatwebsite\Excel\Concerns\FromCollection;
+
+class KuesionerTamu implements FromCollection
+{
+    /**
+    * @return \Illuminate\Support\Collection
+    */
+    public function collection()
+    {
+        return KuesionerTamu::select([
+        'nama',
+        'instansi',
+        'tampilan_produk',
+        'tampilan_stand',
+        'penjelasan_produk',
+        'hiburan',
+        'kritik_saran',
+        ])->get();
+    }
+
+    public function headings() {
+        return [
+            'Nama',
+            'Instansi',
+            'Tampilan Produk',
+            'Penjelasan Produk',
+            'Hiburan',
+            'Kritik & Saran'
+        ]
+    }
+}
